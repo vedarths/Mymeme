f28b37322a50a7b804da0ecfe12ee8b9af53b528
Sudi <vedarthlimited@gmail.com>
2018-04-18 22:24:20 +0100

    added methods to present ui image view controller

--- MyVMeme/ViewController.swift
+++ MyVMeme/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //outlets
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var galleryButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var bottomText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cancelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-
+    @IBAction func pickFromGallery(_ sender: Any) {
+        presentAnImageWithSourceType(sourceType: UIImagePickerControllerSourceType.photoLibrary)
+    }
+    
+    @IBAction func pickFromCamera(_ sender: Any) {
+        presentAnImageWithSourceType(sourceType: UIImagePickerControllerSourceType.camera)
+    }
+    
+    private func presentAnImageWithSourceType(sourceType: UIImagePickerControllerSourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate=self
+            present(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
 
 }
 
